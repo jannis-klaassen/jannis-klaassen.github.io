f11c3b6e7fae5a3e3b6a1b691e79e2f6af85fbf4
jannis-klaassen <76206480+jannis-klaassen@users.noreply.github.com>
2025-04-15 14:36:53 +0200

    Add popout animations

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
-// Navbar responsive menu button
 document.addEventListener("DOMContentLoaded", () => {
+    // Navbar responsive menu button
     const toggleButton = document.querySelector(".menu-toggle");
     const navbarPages = document.querySelector(".navbar-pages");
 
     toggleButton.addEventListener("click", () => {
         navbarPages.classList.toggle("active");
     });
-});
 
 
+    // Animate popouts
+    const popoutElements = document.querySelectorAll(".popout");
+    const popoutObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.remove("popout");
+                entry.target.classList.add("popout-animation");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+    popoutElements.forEach(element => popoutObserver.observe(element));
 
-// Animate skill bars 
-document.addEventListener("DOMContentLoaded", () => {
+
+    // Animate skill bars
     const fills = document.querySelectorAll(".fill");
-
-    const observer = new IntersectionObserver((entries, observer) => {
+    const fillObserver = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 const fill = entry.target;
@@ -23,11 +35,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 observer.unobserve(fill);
             }
         });
-    },
-        {
-            threshold: 0.5, // Trigger when 50% is visible
-        }
-    );
-
-    fills.forEach(fill => observer.observe(fill));
+    }, {
+        threshold: 0.5
+    });
+    fills.forEach(fill => fillObserver.observe(fill));
 });
